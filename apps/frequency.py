93d7e254593666bb28854788b1073e02f0f387e2
Ben <bengetch@gmail.com>
2019-06-03 15:39:08 -0400

    cleanup apps

--- apps/frequency.py
+++ apps/frequency.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
 
     e1 = c.frequency(
         'f1',
-        [1800, 1820, 1840],
+        [1800, 1840],
         'Filtered Text'
     )
 
     freq1 = e1.take_freq(['state'], 'f1')
-    freq1.write_to_json('/Users/ben/Desktop/out.json')
+    freq1.write_to_json(sys.argv[2])
 
     freq2 = e1.take_freq(['education'], 'f2')
 
-    g = graph.GraphFrequency(['/Users/ben/Desktop/out.json', freq2], colors=['black', 'orange']).create_plot().show()
+    g = graph.GraphFrequency([sys.argv[2], freq2], colors=['black', 'orange']).create_plot().show()
 
