c3ebfb1933c0bf8b372866097e6d85f5e6ea645a
Ben <bengetch@gmail.com>
2019-07-02 16:19:08 -0400

    os path fix

--- apps/POL-NLP/graph.py
+++ apps/POL-NLP/graph.py
@@ -19,7 +19,7 @@ def setup_parser():
 def setup_filepaths(input_dir):
 
     for subdir, dirs, files in os.walk(input_dir):
-        return ["{0}/{1}".format(input_dir, file) for file in files]
+        return ["{0}/{1}".format(input_dir, file) for file in files if file[0] != "."]
 
 
 if __name__ == '__main__':
